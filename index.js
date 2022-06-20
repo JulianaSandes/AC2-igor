d24c4a57562daac5cac7a8c46b46bf943bc588b5
juliana ( <84139776+JulianaSandes@users.noreply.github.com>
2022-06-20 10:08:42 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+class Lanche{
+
+    constructor(combo, hamburguer, queijo, batataP, fruta, alface, molho, picles, frango){
+        this.hamburguer = hamburguer;
+        this.queijo = queijo;
+        this.batataP = batataP;
+        this.fruta = fruta;
+        this.alface = alface;
+        this.molho = molho;
+        this.cebola = cebola;
+        this.picles = picles;
+        this.frango = frango;
+        thia.combo = combo;
+    }
+
+combo(batata, bebida){
+   if (batata === true || bebida === true){
+    this.combo = Lanche;
+   }
+}
+
+}
+
+const McLancheAlegre = new Lanche("hamburguer, queijo, batataP, fruta");
+
+const BigLanche = new Lanche("hamburguer, alface, queijo, molho, cebola, picles");
+
+const McHen = new Lanche("frango, alface, queijo, picles");
